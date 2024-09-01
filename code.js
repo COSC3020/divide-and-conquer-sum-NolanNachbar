1167c09d338a6b0fce77ca951064afa22e7173e1
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-01 13:14:16 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,18 @@
-function msum(x, lo, hi) {
-    if (lo === hi) return x[0]; // in the case that the sub array is only 0-1 elements long, will return the value of the element
-    else if (lo + 1 === hi) return x[0] + x[1];
+function msum(a, lo, hi) {
+    if (lo === hi) return a[0]; // in the case that the sub array is only 0-1 elements long, will return the value of the element
+    else if (lo + 1 === hi) return a[0] + a[1];
 
     var mid1 = Math.floor((lo + hi) / 3);
     var mid2 = Math.floor((mid1 + hi) / 2);
 
-    var ls = msum(x, lo, mid1);
-    var ms = msum(x, mid1 + 1, mid2);
-    var rs = msum(x, mid2 + 1, hi);
+    var ls = msum(a, lo, mid1);
+    var ms = msum(a, mid1 + 1, mid2);
+    var rs = msum(a, mid2 + 1, hi);
 
     return ls + ms + rs
 }
 
-function mergesum(x) {
-    x = msum(x, 0, x.length - 1);
-    return x;
+function divideAndConquerSum(a) {
+    a = msum(a, 0, a.length - 1);
+    return a;
 }
