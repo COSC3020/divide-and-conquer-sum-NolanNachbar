78f56cc7be9228912055eff308ff10cae24edf3b
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-01 13:48:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,16 @@
-function msum(a, lo, hi) {
-    if (lo === hi) return a[0]; // in the case that the sub array is only 0-1 elements long, will return the value of the element
-    else if (lo + 1 === hi) return a[0] + a[1];
-
-    var mid1 = Math.floor((lo + hi) / 3);
-    var mid2 = Math.floor((mid1 + hi) / 2);
-
-    var ls = msum(a, lo, mid1);
-    var ms = msum(a, mid1 + 1, mid2);
-    var rs = msum(a, mid2 + 1, hi);
-
-    return ls + ms + rs
-}
-
 function divideAndConquerSum(a) {
-    a = msum(a, 0, a.length - 1);
-    return a;
+    if (a.length === 0) return 0;
+    else if (a.length === 1) return a[0];
+
+    // Divide the array into three sub arrays
+    var mid1 = Math.floor((a.length) * 1/3);
+    var mid2 = Math.floor((a.length) * 2/3);
+    
+    // Sum up the subarrays recurssively
+    var sum1 = divideAndConquerSum(a.slice(0,mid1));
+    var sum2 = divideAndConquerSum(a.slice(mid1,mid2));
+    var sum3 = divideAndConquerSum(a.slice(mid2,a.length));
+
+    // Sum up the other sums
+    return sum1 + sum2 + sum3;
 }
