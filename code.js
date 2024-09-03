67b11793be46674acb59c17228cd59df2c001505
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-02 18:26:40 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 function divideAndConquerSum(a) {
     // Handle the trivial cases
-    if (a.length === 0) return 0;
-    else if (a.length === 1) return a[0];
+    if (a.length == 0) return 0;
+    else if (a.length == 1) return a[0];
 
-    // Divide the array into three sub arrays
-    var mid1 = Math.floor(a.length * 1/3);
-    var mid2 = Math.floor(a.length * 2/3);
+    // Divide the array into three sub-arrays
+    var mid1 = Math.floor((1 / 3) * a.length);
+    var mid2 = Math.floor((2 / 3) * a.length);
     
-    // Sum up the subarrays recursively
+    // Sum up the sub-arrays recursively
     var sum1 = divideAndConquerSum(a.slice(0, mid1));
     var sum2 = divideAndConquerSum(a.slice(mid1, mid2));
     var sum3 = divideAndConquerSum(a.slice(mid2, a.length));
