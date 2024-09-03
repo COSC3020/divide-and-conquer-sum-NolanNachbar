569c6e624b57539e84e2fe17fb350ac747fea30d
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-02 18:14:37 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 function divideAndConquerSum(a) {
+    // Handle the trivial cases
     if (a.length === 0) return 0;
     else if (a.length === 1) return a[0];
 
@@ -8,8 +9,8 @@ function divideAndConquerSum(a) {
     
     // Sum up the subarrays recurssively
     var sum1 = divideAndConquerSum(a.slice(0,mid1));
-    var sum2 = divideAndConquerSum(a.slice(mid1,mid2));
-    var sum3 = divideAndConquerSum(a.slice(mid2,a.length));
+    var sum2 = divideAndConquerSum(a.slice(mid1 + 1,mid2));
+    var sum3 = divideAndConquerSum(a.slice(mid2 + 1,a.length));
 
     // Sum up the other sums
     return sum1 + sum2 + sum3;
