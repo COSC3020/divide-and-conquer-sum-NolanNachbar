c27c092699ce90f97c42b2a976b9f935afd375d3
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-03 17:10:17 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,33 @@
-function divideAndConquerSum(a) {
-    // Handle the trivial cases
-    if (a.length === 0) return 0;
-    if (a.length === 1) return a[0];
-
-    // Divide the array into three sub-arrays
-    const mid1 = Math.floor((a.length + 1) /3);
-    const mid2 = Math.floor(2 * (a.length + 1) / 3); 
-
+function DivideandSumHelper(arr, start, end) {
     
-    const sum1 = divideAndConquerSum(a.slice(0, mid1));
-    const sum2 = divideAndConquerSum(a.slice(mid1, mid2));
-    const sum3 = divideAndConquerSum(a.slice(mid2));
+    //If an array has only one element
+    if (start === end) {
+        return arr[start]; 
+    }
+    
+    //If there are no elements 
+    if (start > end) {
+        return 0; 
+    }
 
-    // Sum up the other sums
-    return sum1 + sum2 + sum3;
+   //help split the array into three parts 
+    const first = start + Math.floor((end-start + 1) /3);
+    const second = start + Math.floor(2 * (end - start + 1) / 3); 
+
+    // will find the sum recursively
+    const sum1 = DivideandSumHelper(arr, start, first - 1);
+    const sum2 = DivideandSumHelper(arr, first, second - 1); 
+    const sum3 = DivideandSumHelper(arr, second, end); 
+
+    //Merge them all together
+    return sum1 + sum2 + sum3; 
+
+}
+    
+
+function divideAndConquerSum(a) {
+    if (!Array.isArray(a) || a.length === 0) {
+        return 0; 
+    }
+    return DivideandSumHelper(a, 0, a.length -1);
 }
