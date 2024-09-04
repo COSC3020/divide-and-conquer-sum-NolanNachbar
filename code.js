129af1a727aa03b61af9b444ac4845d80250425e
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-04 15:10:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function divideAndConquerSum(a) {
-    return a;
+    // Handle the trivial cases
+    if (a.length === 0) return 0;
+    if (a.length === 1) return a[0];
+
+    // Divide the array into three sub-arrays
+    const mid1 = Math.floor((a.length + 1) /3);
+    const mid2 = Math.floor(2 * (a.length + 1) / 3); 
+
+    
+    const sum1 = divideAndConquerSum(a.slice(0, mid1));
+    const sum2 = divideAndConquerSum(a.slice(mid1, mid2));
+    const sum3 = divideAndConquerSum(a.slice(mid2));
+
+    // Sum up the other sums
+    return sum1 + sum2 + sum3;
 }
